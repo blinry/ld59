3eba6fde0caabe12dbf27ca8d0ee1bdbb5cdaf01
blinry <mail@blinry.org>
2026-04-18 17:26:50 +0200

    Little scene with lighthouse and two ships

---
+++ ship.gd
@@ -0,0 +1,13 @@
+extends CharacterBody3D
+
+@export var speed: float = 3 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	velocity = transform.basis.z * speed
+	move_and_slide()
