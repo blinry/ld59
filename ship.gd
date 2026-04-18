563e4e3121475a2ca8ba0de3c7d1a9fac03d1267
blinry <mail@blinry.org>
2026-04-18 21:20:02 +0200

    Ships can crash

--- ship.gd
+++ ship.gd
@@ -1,7 +1,9 @@
 extends CharacterBody3D
 
-@export var speed: float = 5
+@export var speed: float = 10
 @export_range(0.0,0.5) var rotation_speed: float = 0.005
+
+var crashed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +13,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	velocity = transform.basis.z * speed
-	move_and_slide()
+	var collision = move_and_slide()
+	if collision:
+		explode()
 
 func steer(attractor, delta, repel):
 	var target_angle =  global_position.direction_to(attractor.global_position).signed_angle_to(transform.basis.z,Vector3.DOWN)
@@ -19,3 +23,15 @@ func steer(attractor, delta, repel):
 		target_angle += PI
 	#rotate_y(deg_to_rad(max_rotation_speed*delta))
 	rotation.y = lerp_angle(rotation.y, rotation.y + target_angle, rotation_speed)
+
+func explode():
+	if not crashed:
+		print("boom!")
+		speed = 0
+		var black = StandardMaterial3D.new()
+		black.albedo_color = Color(0, 0, 0)
+		black.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+		black.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+		$Body.set_surface_override_material(0, black)
+		$Front.set_surface_override_material(0, black)
+		crashed = true
