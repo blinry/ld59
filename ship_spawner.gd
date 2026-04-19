a159b198f2f8b10e5db59fb5dadc8b1edf4c1b7e
blinry <mail@blinry.org>
2026-04-19 14:58:54 +0200

    Give spawners an interval

--- ship_spawner.gd
+++ ship_spawner.gd
@@ -1,11 +1,12 @@
 extends Node3D
 
+@export var interval = 20 # seconds
+
 const SHIP = preload("uid://de42481q0cwhl")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$Timer.wait_time = interval
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
